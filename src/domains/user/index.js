94ac47bdacbeb8afdced431e09b85f6a122e404a
starsoftdev <26078620+starsoftdev@users.noreply.github.com>
2018-09-05 00:15:11 -0400

    added domain object of user

---
+++ src/domains/user/index.js
@@ -0,0 +1,85 @@
+
+
+//// TODO: use Redux to observe the data of user
+class User {
+
+  constructor(userId){
+
+    this._userId = userId
+
+  }
+
+  get profilePicture(){
+
+  }
+
+  get firstName(){
+
+  }
+
+  get lastName(){
+
+  }
+
+  get summary(){
+
+  }
+
+  get address() {
+
+  }
+
+  get joinDate() {
+
+  }
+
+  get averageResponseTime() {
+
+  }
+
+  get recentDelivery() {
+
+  }
+
+  get description() {
+
+  }
+
+  get languages() {
+
+  }
+
+  get skills() {
+
+  }
+
+  get organizations() {
+
+  }
+
+  get socialMediaProfiles() {
+
+  }
+
+  get certifications() {
+
+  }
+
+  get userServices() {
+
+  }
+
+  get portfolio() {
+
+  }
+
+  get notableProjects() {
+
+  }
+
+  get reviews() {
+
+
+  }
+
+}
